106b0612775cac09f67d5501a64487bb164786c7
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-12 00:53:08 +0300

    CreateProduct

--- MyApiNight4.WebUI/Controllers/CategoriesController.cs
+++ MyApiNight4.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyApiNight4.WebUI.Dtos;
 using Newtonsoft.Json;
+using System.Diagnostics.Contracts;
 using System.Text;
 
 namespace MyApiNight4.WebUI.Controllers
@@ -48,12 +49,38 @@ namespace MyApiNight4.WebUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.DeleteAsync("https://localhost:7039/api/Category?id=" + id);
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("CategoryList");
             }
             return View();
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7039/api/Category/GetCategory?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7039/api/Category/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CategoryList");
+            }
+            return View();
         }
     }
 }
